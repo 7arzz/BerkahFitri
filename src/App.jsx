2d78d48122b03f994a529f43ba1cfd15f135ce05
7arzz <7arzz404@gmail.com>
2026-02-27 16:49:58 +0700

    lentera

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Greeting from "./components/Greeting";
 import SupportButton from "./components/SupportButton";
 import "./App.css";
+import LenteraRamadhan from "./assets/lenteraRamadhan.png";
 
 function App() {
   return (
@@ -8,6 +9,26 @@ function App() {
       className="container"
       style={{ justifyContent: "center", alignItems: "center" }}
     >
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          display: "flex",
+          gap: "auto",
+          justifyContent: "space-between",
+        }}
+      >
+        <img
+          src={LenteraRamadhan}
+          alt="Lentera Ramadhan"
+          style={{ width: "30%", display: "flex", top: "0", left: "0" }}
+        />
+        <img
+          src={LenteraRamadhan}
+          alt="Lentera Ramadhan"
+          style={{ width: "30%", display: "flex", top: "0", right: "0" }}
+        />
+      </div>
       <Greeting />
       <SupportButton />
     </div>
